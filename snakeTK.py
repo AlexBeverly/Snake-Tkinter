091863210a2613a174e60e1d01d7ddbb8e6966c7
tumbleweeding <cloneop800@gmail.com>
2020-06-10 11:12:01 -0700

    show snake head as darker green. snake moves faster as it grows. message box popup on Game Over

--- snakeTK.py
+++ snakeTK.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from snake import snake
 
 # tkinter window
@@ -8,8 +9,12 @@ root = Tk()
 board_size = 400
 cell_size = board_size/10
 isRunning = False
+# changes how much faster snake moves as it gets larger
+snake_speed_multiplier = 100
 
 s = snake()
+# save the starting length of the snake for later
+start_len = s.snake_len
 board = Canvas(root, height=board_size + 2, width=board_size + 3)
 board.pack()
 
@@ -20,7 +25,11 @@ def draw_snake():
             if s.grid[row][col] < 0:    # apple
                 color = 'red'
             elif s.grid[row][col] > 0:  # snake
-                color = 'green2'
+                #check if tile is the head of the snake
+                if (row,col) == tuple(s.snake_pos):
+                    color = 'green4'
+                else:
+                    color = 'green2'
             else:                       # empty
                 color = 'snow'
 
@@ -42,9 +51,15 @@ def step():
     state = s.move()
     draw_snake()
     if state != 0:
-        board.after(1000,step)
+        # subtract wait time from 1 second as the snake gets larger
+        wait_time = 1000 - ((s.snake_len - start_len) * snake_speed_multiplier)
+        # keep time between steps at 200 or above
+        if wait_time < 200:
+            wait_time = 200
+        board.after(wait_time, step)
     else:
         isRunning = False
+        messagebox.showerror('GAME OVER', 'GAME OVER!')
 
 def change_dir(new_dir):
     global isRunning
